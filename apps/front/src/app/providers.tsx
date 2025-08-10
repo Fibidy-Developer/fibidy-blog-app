1c350954a701867e1a3b85c807fa8d09ba351445
Fibidy Dev <fibidyapp@gmail.com>
2025-08-11 03:16:48 +0700

    feat: trigger vercel build test

--- apps/front/src/app/providers.tsx
+++ apps/front/src/app/providers.tsx
@@ -6,7 +6,6 @@ import { PropsWithChildren } from "react";
 const queryClient = new QueryClient();
 
 type Props = PropsWithChildren;
-
 const Providers = ({ children }: Props) => {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
